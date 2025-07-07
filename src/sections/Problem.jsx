a7cc2571bfce840e9c2a167b7cd80ead201c7b6e
Jordan Kline <jordan.kline@gmail.com>
2025-07-06 19:45:51 -0700

    add assets to page

--- src/sections/Problem.jsx
+++ src/sections/Problem.jsx
@@ -1,3 +1,5 @@
+import { Stack } from '@mui/material';
+
 import { Section } from '../containers';
 
 import './Problem.css';
@@ -5,6 +7,11 @@ import './Problem.css';
 const Problem = () => (
     <Section id="problem" title="Problem">
       <p>In San Francisco, homeowners are required to cover the drain pipes in front of their homes with grates to prevent litter from entering the pipes. These covers are made of steel and they a frequently stolen. Historically, they were made of brass.</p>
+
+      <Stack direction="row" spacing={2} sx={{ width: '100%' }}>
+        <img style={{ width: '49%' }} src="public/uncovered-sewer-cap-type-1.jpg" alt="Uncovered sewer cap type 1" />
+        <img style={{ width: '49%' }} src="public/uncovered-sewer-cap-type-1-a.jpg" alt="Uncovered sewer cap type 2" />
+      </Stack>
       <p>There are a few reasons that these covers are frequently stolen:</p>
       <ul>
         <li>The covers are made of recyclable materials</li>
