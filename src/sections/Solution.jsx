63b4cc7315d3aa56c5ab01a6211ae1d04e9339ee
jordaninthewind <jordan.kline@gmail.com>
2025-10-17 15:02:37 -0700

    add asset

--- src/sections/Solution.jsx
+++ src/sections/Solution.jsx
@@ -12,18 +12,19 @@ const Solution = () => (
     <Grid container spacing={4}>
       <Grid item size={{ xs: 12, md: 5 }}>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+          <img src="cover-logo.png" style={{ maxWidth: '100%', width: 'auto', height: 'auto' }} alt="Cover logo" />
           <Typography variant="sectionTitle">{title}</Typography>
           <Divider sx={{ border: '3px solid #000', mb: 2 }} />
-          <Typography variant="subtitle">{subtitle}</Typography>
         </Box>
       </Grid>
       <Grid item size={{ xs: 12, md: 7 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Typography variant="subtitle">{subtitle}</Typography>
           <img src="sewer-cover-type-1.jpg" style={{ maxWidth: '100%', width: 'auto', height: 'auto' }} alt="Printed sewer cover" />
         </Box>
       </Grid>
       <Grid item size={{ xs: 12 }}>
-        <Typography variant="h5">We have designed and are actively printing a cheap 3D printed version of the sewer cover to let anyone print their own and cover up theirs or their neighbor's sewer cover.</Typography>
+        <Typography variant="h4">We have designed and are actively printing a cheap 3D printed version of the sewer cover to let anyone print their own and cover up theirs or their neighbor's sewer cover.</Typography>
       </Grid>
     </Grid>
   </Section>
