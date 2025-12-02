d581fcbfc829eebe071fc9bb08ee55d094133c0c
jordaninthewind <jordan.kline@gmail.com>
2025-12-02 14:50:58 -0800

    split gradient button component

---
+++ src/components/GradientButton/GradientButton.jsx
@@ -0,0 +1,16 @@
+import { Button, styled } from "@mui/material";
+
+const GradientButton = styled(Button)({
+    background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
+    color: "white",
+    minWidth: "fit-content",
+    padding: "1rem 2rem",
+    "&:hover": {
+        color: "white",
+        background: "linear-gradient(55deg, #667eea 0%, #764ba2 100%)",
+        transform: "scale(1.05)",
+        transition: "background 1s ease-in-out, transform 0.2s ease-in-out",
+    },
+});
+
+export default GradientButton;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Copywrite } from "@app/components/Copywrite/Copywrite";
 export { default as DashedDivider } from "@app/components/DashedDivider/DashedDivider";
 export { default as Header } from "@app/components/Header/Header";
+export { default as GradientButton } from "@app/components/GradientButton/GradientButton";
 export { default as HoverActionCard } from "@app/components/HoverActionCard/HoverActionCard";
 export { default as InteractiveMap } from "@app/components/InteractiveMap/InteractiveMap";
 export { default as ModelViewer } from "@app/components/ModelViewer/ModelViewer";
