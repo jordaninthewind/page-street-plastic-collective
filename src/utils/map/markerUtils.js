a44d542740731fbe818cfed6711076b2ba56b058
jordaninthewind <jordan.kline@gmail.com>
2025-12-22 15:07:37 -0800

    update markers, move cover image to assets

--- src/utils/map/markerUtils.js
+++ src/utils/map/markerUtils.js
@@ -1,28 +1,31 @@
 import { Marker } from "mapbox-gl";
 
+import CoverLogo from "@app/assets/cover-logo.svg";
 import { MARKER_TYPES } from "@app/constants";
 
 export const createMarker = (marker, onClick = () => {}) => {
-  const { id, lng, lat, state } = marker;
+  const { id, lng, lat, state = "temporary" } = marker;
 
   const el = document.createElement("div");
 
+  const style = {
+    backgroundColor: MARKER_TYPES[state].color,
+    cursor: "pointer",
+    zIndex: 1000,
+    width: "80px",
+    height: "80px",
+    padding: "10px",
+    objectFit: "contain",
+    borderRadius: "50%",
+  };
+
   el.innerHTML = `
-    <div>
-      <h3>Marker ${id}</h3>
-      <p>State: ${state || "temporary"}</p>
-    </div>
+    <img src="${CoverLogo}" alt="Marker ${id}" style="width: 100%; height: 100%;" />
   `;
 
-  el.style.backgroundColor = MARKER_TYPES[state]?.color || "red";
-  el.style.padding = "10px";
-  el.style.borderRadius = "5px";
-  el.style.cursor = "pointer";
-  el.style.zIndex = 1000;
+  Object.assign(el.style, style);
 
-  el.addEventListener("click", () => {
-    onClick(marker);
-  });
+  el.addEventListener("click", () => onClick(marker));
 
   return new Marker(el).setLngLat([lng, lat]);
 };
