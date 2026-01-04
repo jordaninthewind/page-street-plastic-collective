95a0ff8aab7a3ff8f9f65f8e3d32e8b7a38da2f2
jordaninthewind <jordan.kline@gmail.com>
2026-01-03 22:49:50 -0800

    fix select component

--- src/components/Map/AddressSearch/AddressSearch.jsx
+++ src/components/Map/AddressSearch/AddressSearch.jsx
@@ -1,28 +1,44 @@
-import { MenuItem, Select } from "@mui/material";
+import { useCallback, useState } from "react";
+
+import { Box, MenuItem, Select } from "@mui/material";
 
 import { useSearchParamState } from "@app/hooks";
 import { useMapStore } from "@app/stores";
 
 const AddressSearch = () => {
+  const { id, setParams } = useSearchParamState();
+
   const { markers } = useMapStore();
 
-  const { setParams } = useSearchParamState();
+  const [selectedId, setSelectedId] = useState(id || "");
+
+  const handleChange = useCallback(
+    ({ target: { value } }) => {
+      setSelectedId(value);
+
+      const marker = markers.find((marker) => marker.id === Number(value));
+
+      if (!marker) return;
+
+      setParams({ id: marker.id, lat: marker.lat, lng: marker.lng });
+    },
+    [markers, setParams]
+  );
 
   return (
-    <Select fullWidth>
-      <MenuItem value="" disabled>
-        Select a marker to navigate and show info
-      </MenuItem>
-      {markers?.map(({ id, lat, lng, address }) => (
-        <MenuItem
-          key={id}
-          onClick={() => setParams({ id, lat, lng })}
-          value={id}
-        >
-          {address}
+    <Box sx={{ width: "100%" }}>
+      <Select fullWidth onChange={handleChange} value={selectedId} displayEmpty>
+        <MenuItem value="">
+          {" "}
+          Select a marker to navigate and show info{" "}
         </MenuItem>
-      ))}
-    </Select>
+        {markers?.map(({ id, address }) => (
+          <MenuItem key={id} value={id}>
+            {address}
+          </MenuItem>
+        ))}
+      </Select>
+    </Box>
   );
 };
 
