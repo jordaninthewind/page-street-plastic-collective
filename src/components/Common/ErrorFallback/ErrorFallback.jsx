b906c1015e42889ee606bbf8928270031737dd34
jordaninthewind <jordan.kline@gmail.com>
2026-01-03 23:54:30 -0800

    add mobile handling to fallback

--- src/components/Common/ErrorFallback/ErrorFallback.jsx
+++ src/components/Common/ErrorFallback/ErrorFallback.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, Link, Typography } from "@mui/material";
 
 import FallbackImage from "@app/assets/fallback-image.png";
+import { useIsMobile } from "@app/hooks";
 
 const env = import.meta.env.VITE_ENVIRONMENT;
 
@@ -9,125 +10,126 @@ const ErrorFallback = ({
   resetErrorBoundary,
   componentStack,
   eventId,
-}) => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    justifyContent="center"
-    minHeight="100vh"
-    p={3}
-    sx={{ backgroundColor: "#fefefe" }}
-  >
-    <Typography variant="h5" color="error" gutterBottom>
-      Let's try that again..
-    </Typography>
-    <img
-      src={FallbackImage}
-      alt="Fallback"
-      style={{ width: "70%", height: "100%" }}
-    />
-    <Typography
-      variant="body1"
-      color="text.secondary"
-      align="center"
-      sx={{ mb: 2 }}
-    >
-      An unexpected error occurred. Please try refreshing the page or{" "}
-      <Link href="mailto:support@page-street-plastic-collective.com">
-        contact us
-      </Link>{" "}
-      if the problem persists. support if the problem persists.
-    </Typography>
-    <Button
-      variant="outlined"
-      color="primary"
-      onClick={() => resetErrorBoundary?.() || window.location.reload()}
-      sx={{ m: 4 }}
-    >
-      Refresh the page
-    </Button>
+}) => {
+  const { isMobile } = useIsMobile();
 
-    {env === "development" && error && (
-      <Box
-        sx={{
-          mt: 2,
-          p: 2,
-          backgroundColor: "#f0f0f0",
-          borderRadius: 1,
-          width: "100%",
-          maxWidth: "600px",
-        }}
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+      p={3}
+      sx={{ backgroundColor: "#fefefe" }}
+    >
+      <Typography variant="h5" color="error" gutterBottom>
+        Let's try that again..
+      </Typography>
+      <img
+        src={FallbackImage}
+        alt="Fallback"
+        style={{ width: isMobile ? "100%" : "70%", height: "100%" }}
+      />
+      <Typography
+        variant="body1"
+        color="text.secondary"
+        align="center"
+        sx={{ mb: 2 }}
       >
-        <Typography variant="h6" color="error">
-          Error Details (Development Mode):
-        </Typography>
-        <Typography
-          variant="body2"
-          component="pre"
-          sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+        An unexpected error occurred. Please try refreshing the page or{" "}
+        <Link href="mailto:hi@pagestreetplasticcollective.com">contact us</Link>{" "}
+        if the problem persists. support if the problem persists.
+      </Typography>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => resetErrorBoundary?.() || window.location.reload()}
+        sx={{ m: 4 }}
+      >
+        Refresh the page
+      </Button>
+
+      {env === "development" && error && (
+        <Box
+          sx={{
+            mt: 2,
+            p: 2,
+            backgroundColor: "#f0f0f0",
+            borderRadius: 1,
+            width: "100%",
+            maxWidth: "600px",
+          }}
         >
-          {error.toString()}
-        </Typography>
-        {error.stack && (
+          <Typography variant="h6" color="error">
+            Error Details (Development Mode):
+          </Typography>
           <Typography
             variant="body2"
             component="pre"
             sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
           >
-            {error.stack}
+            {error.toString()}
           </Typography>
-        )}
-      </Box>
-    )}
+          {error.stack && (
+            <Typography
+              variant="body2"
+              component="pre"
+              sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+            >
+              {error.stack}
+            </Typography>
+          )}
+        </Box>
+      )}
 
-    {env === "development" && componentStack && (
-      <Box
-        sx={{
-          mt: 2,
-          p: 2,
-          backgroundColor: "#f0f0f0",
-          borderRadius: 1,
-          width: "100%",
-          maxWidth: "600px",
-        }}
-      >
-        <Typography variant="h6" color="error">
-          Component Stack (Development Mode):
-        </Typography>
-        <Typography
-          variant="body2"
-          component="pre"
-          sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+      {env === "development" && componentStack && (
+        <Box
+          sx={{
+            mt: 2,
+            p: 2,
+            backgroundColor: "#f0f0f0",
+            borderRadius: 1,
+            width: "100%",
+            maxWidth: "600px",
+          }}
         >
-          {componentStack}
-        </Typography>
-      </Box>
-    )}
+          <Typography variant="h6" color="error">
+            Component Stack (Development Mode):
+          </Typography>
+          <Typography
+            variant="body2"
+            component="pre"
+            sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+          >
+            {componentStack}
+          </Typography>
+        </Box>
+      )}
 
-    {env === "development" && eventId && (
-      <Box
-        sx={{
-          mt: 2,
-          p: 2,
-          backgroundColor: "#f0f0f0",
-          borderRadius: 1,
-          width: "100%",
-          maxWidth: "600px",
-        }}
-      >
-        <Typography variant="h6" color="error">
-          PostHog Event ID (Development Mode):
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+      {env === "development" && eventId && (
+        <Box
+          sx={{
+            mt: 2,
+            p: 2,
+            backgroundColor: "#f0f0f0",
+            borderRadius: 1,
+            width: "100%",
+            maxWidth: "600px",
+          }}
         >
-          {eventId}
-        </Typography>
-      </Box>
-    )}
-  </Box>
-);
-
+          <Typography variant="h6" color="error">
+            PostHog Event ID (Development Mode):
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{ fontFamily: "monospace", fontSize: "0.75rem", mt: 1 }}
+          >
+            {eventId}
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+};
 export default ErrorFallback;
